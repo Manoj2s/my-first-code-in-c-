84f90a9ba23553a15b4b8a796ed81460047a9a1b
Manoj2s <152688833+Manoj2s@users.noreply.github.com>
2023-12-11 23:10:15 +0530

    Add files via upload

---
+++ if.c
@@ -0,0 +1,48 @@
+// programe for greatest number
+// #include<stdio.h>
+// int main(){
+//     int a,b;
+//     scanf("%d %d",&a,&b);
+//     if (a>b){
+//         printf("%d",a);
+//     }else {
+//         printf("%d",b);
+//     }
+// }
+
+// Write a program to take two numbers A and B as input from the user and print the number closest to (but less than) A which is completely divisible by B.
+
+// #include <stdio.h>
+// int main()
+// {
+//     int a, b;
+//     scanf("%d %d", &a, &b);
+//     if (a > b)
+//     {
+//         int c;
+//         scanf("%d", &c);
+//         if (c < a)
+//         {
+//             if (c % b == 0)
+//             {
+//                 printf("%d", c);
+//             }
+//             else
+//             {
+//                 printf("c is not divisible by b");
+//             }
+//         }
+//         else
+//         {
+//             printf("a is greater than c");
+//         }
+//     }
+//     else{
+//         printf("b is greater than a");
+//     }
+
+//     return 0;
+// }
+
+
+
