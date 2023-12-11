daa71aa53c9f05ebc9fdab7088d1210bc18a0549
Manoj2s <152688833+Manoj2s@users.noreply.github.com>
2023-12-11 23:11:23 +0530

    Add files via upload
    operators

---
+++ opt.c
@@ -0,0 +1,137 @@
+// programe for sum of two numbers
+
+// #include<stdio.h>
+// int main(){
+//     int a,b;
+//     printf("Enter the first value of the number:->");
+//     scanf("%d",&a);
+//     printf("Enter the value of the number second:->");
+//     scanf("%d",&b);
+//     int c;
+//     c=a+b;
+//     printf("Sum of the number :-> %d\n",c);
+//     return 0;
+// }
+
+
+// 
+// #include<stdio.h>
+// int main(){   
+//     int a,b;
+//     scanf("%d",&a);
+//     scanf("%d",&b);
+//     int c;
+//     c=a+b;#include<stdio.h>
+//     printf("%d\n",c);
+//     return 0;
+// }
+
+
+//   programe forn difference of two numbers
+
+// #include<stdio.h>
+// int main (){
+//     int a,b;
+//     scanf("%d",&a);
+//     scanf("%d",&b);
+//     int d;
+//     d=a-b;
+//     printf("%d\n",d);
+//     return 0;
+// }
+
+
+// Write a program to show the procedure for obtaining the average of two given numbers.(Input 2 numbers from the user)
+
+// #include<stdio.h>
+// int main(){
+//     float a, b;
+//     printf("enter first num....>");
+//     scanf("%f",&a);
+//     printf("enter second num...>");
+//     scanf("%f",&b);
+//     float c;
+//     c=(a+b)/2;
+//     printf("%f\n",c);
+//     return 0;
+
+// }
+
+
+// Write a program to show how to obtain the daily wage of a worker on the basis of the hours worked during the day.(Daily wage of a worker is determined by hours worked per day and the hourly wage rate. Input the number of hours worked from the user and the hourly wage rate)
+
+// #include<stdio.h>
+// int main(){
+//     int d,h,w;
+//     scanf("%d %d %d",&d,&h,&w);
+//     int i;
+//     i= d*h*w;
+//     printf("%d",i);
+//     return 0;
+
+// }
+
+
+// Write a program to show how to obtain the area of a triangle on the basis of the base and height.(Input the base and height of the triangle from the user)
+
+// #include<stdio.h>
+// int main(){
+//     int b, h;
+//     scanf("%d%d",&b,&h);
+//     int a;
+//     a=(b*h)*1/2;
+//     printf("%d\n",a);
+//     return 0;
+// }
+
+// Write a program to show the steps in finding the simple interest on a given amount at a given rate of interest.(Input amount, rate of interest and time from the user)
+
+// #include<stdio.h>
+// int main(){
+//     int p,r,t;
+//     scanf("%d %d %d",&p,&r,&t);
+//     int a;
+//     a= (p*r*t)/100;
+    // printf("principal ammount......%d\n",p+a);   
+//     printf("intrest ammount......%d\n",a);
+//     return 0;
+// }
+
+
+// Write a program to show how to interchange the values of two variables.
+// #include<stdio.h>
+// int main(){
+//     int a,b;
+//     int t=0;
+//     scanf("%d %d", &a,&b);
+//     t=a;
+//     i= d*h*w;
+//     printf("%d",i);
+//     return 0;
+
+// }
+
+//     a=b;
+//     b=t;
+//     printf("%d %d",a,b);
+//     return 0;
+
+// // }
+
+// // Write a program to print a Welcome message. Take input from the user and on the output window print ‘Welcome ____’
+// #include<stdio.h>
+// int main(){
+//     char name[15];
+//     scanf("%s",name);
+//     printf("welcome...%s",name);
+//     return 0;
+// }
+
+
+#include <stdio.h>
+int y=20;
+int main(){
+    int b=30;
+    printf("%d %d",b,y);
+    return 0;
+}
